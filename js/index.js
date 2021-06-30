69ad25d8db4ff64a0d0d34653219ce1898093ddb
ErickMUOSD <martinoli944@gmail.com>
2021-06-30 10:04:38 -0500

    Renderiza la derivada, extrae la cadena por signos

---
+++ js/index.js
@@ -0,0 +1,71 @@
+
+var derivada_entrada_ = document.getElementById("derivada_entrada");
+
+derivada_entrada_.addEventListener('input', function updateValue(e) {
+
+    mostrarEcuacion(e.target.value);
+});
+
+
+function mostrarEcuacion(derivada_entrada) {
+    // var derivada_entrada_ = document.getElementById("derivada_entrada");
+    var derivada_salida_ = document.getElementById("derivada_salida");
+
+    derivada_salida_.innerHTML = ''
+    MathJax.texReset();
+    var options = MathJax.getMetricsFor(derivada_salida_);
+    MathJax.tex2chtmlPromise(derivada_entrada, options).then(function (node) {
+
+        derivada_salida_.appendChild(node);
+        MathJax.startup.document.clear();
+        MathJax.startup.document.updateDocument();
+    }).catch(function (err) {
+
+        output.appendChild(document.createElement('pre')).appendChild(document.createTextNode(err.message));
+    });
+}
+
+
+function resolverEcuacion() {
+
+    let derivada_entrada_ = document.getElementById("derivada_entrada").value;
+    let arreglo = derivada_entrada_.split(/(?=[-+*\/])/);
+    let arregloInt = [];
+    let containsWords = /[-+\][a-zA-Z]/g
+    let exponete, termino, expontentD, terminoD, resultado = "";
+    console.log(arreglo)
+    //10x^2+1x-15-1-3x+15^5
+    //10x^4-15x^4
+    for (let i = 0; i < arreglo.length; i++) {
+        arregloInt[i] = arreglo[i].match(/-?\d+/g);
+        console.log(arregloInt[i]);
+        if (arreglo[i].includes("^") && containsWords.test(arreglo[i])) {
+            termino = parseInt(arregloInt[i])
+            exponete = parseInt(arregloInt[i][1])
+            terminoD = termino * exponete
+            expontentD = exponete - 1
+            if (expontentD == 1) {
+                console.log("sub 1 El resultado es  " + terminoD + "x")
+                resultado += terminoD + "x"
+            }
+            else {
+                console.log("sub2 El resultado es: " + terminoD + "x^" + expontentD)
+                resultado += terminoD + "x^" + expontentD
+            }
+
+        } if (containsWords.test(arreglo[i]) && !arreglo[i].includes("^")) {
+
+            resultado += arreglo[i]
+            console.log("sub3 " + arregloInt[i] + "X")
+        }
+        if(!containsWords.test(arreglo[i])){
+
+        }
+
+    }
+
+    console.log(resultado)
+}
+
+
+
